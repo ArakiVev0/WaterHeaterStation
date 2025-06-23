0771fc0ac6969555bed401d4de15a051427b5c1d
eyad <certifiedcoffelover@gmail.com>
2025-06-23 12:48:15 -0700

    added stuff

--- max1239.py
+++ max1239.py
@@ -5,13 +5,13 @@ import time
 
 
 class InputMode(Enum):
-    SingalMode = 1
-    DifferentialMode = 0
+    SingleEnded = 1
+    Differential = 0
 
 
 class ClockType(Enum):
-    ExternalClock = 0
-    InternalClock = 1
+    External = 0
+    Internal = 1
 
 
 class Polarity(Enum):
@@ -19,69 +19,104 @@ class Polarity(Enum):
     Bipolar = 1
 
 
-class Reset(Enum):
-    ResetConfig = 0
+class ResetMode(Enum):
+    Reset = 0
     NoAction = 1
 
 
+class ScanMode(Enum):
+    NoScan = 0b00
+    ScanUpToChannel = 0b01
+    ScanUpToAndWrap = 0b10
+    ScanAll = 0b11
+
+
+class RefrenceVoltage(Enum):
+    VDD_Analog_input = 0b000
+    ExternalReference = 0b010
+    InternalReference_AlwaysOFF_AnalogInput = 0b100
+    InternalReference_AlwaysON_AnalogInput = 0b101
+    InternalReference_AlwaysOFF_ReferenceOutput = 0b110
+    InternalReference_AlwaysON_ReferenceOutput = 0b111
+
+
 class Max1239:
-    def __init__(self, address=0x35) -> None:
+    def __init__(self, address: int = 0x35, bus_num: int = 1) -> None:
         self.address = address
-        self.bus = SMBus(0)
+        self.bus = SMBus(bus_num)
 
-    def create_config_binary(
+    def _build_setup_byte(
         self,
-        scan: int,
-        channel: int,
-        inputMode: InputMode,
-    ) -> int:
-        if scan > 3 or scan < 0:
-            raise ValueError("Invalid Scan value should be 0 to 3")
-        if channel > 11 or channel < 0:
-            raise ValueError("Invalid Channel value should be 0 to 11")
-        return 0 << 7 | scan << 6 | channel << 4 | inputMode.value
-
-    def create_setup_binary(
-        self, select: int, clockType: ClockType, polarity: Polarity, reset: Reset
+        referenceVoltage: RefrenceVoltage,
+        clock: ClockType,
+        polarity: Polarity,
+        reset: ResetMode,
     ) -> int:
         return (
-            1 << 7
-            | select << 6
-            | clockType.value << 3
-            | polarity.value << 2
-            | reset.value << 1
+            (1 << 7)  # Setup command
+            | (referenceVoltage.value << 6)
+            | (clock.value << 3)
+            | (polarity.value << 2)
+            | (reset.value << 1)
             | 0
         )
 
-    def read_channel(
+    def _build_config_byte(
         self,
-        scan: int = 0,
-        channel: int = 0,
-        inputMode: InputMode = InputMode.SingalMode,
-    ) -> Optional[int]:
-        try:
-            config = self.create_config_binary(scan, channel, inputMode)
-            self.bus.write_byte(self.address, config)
+        scan: ScanMode,
+        channel: int,
+        mode: InputMode,
+    ) -> int:
+        if not 0 <= channel <= 11:
+            raise ValueError("Channel must be between 0 and 11")
 
-        except ValueError as e:
-            print(e)
-            return None
-
-        time.sleep(0.001)
-
-        res = self.bus.read_i2c_block_data(self.address, 0x00, 2)
-        return (res[0] << 4) | (res[1] >> 4)
+        return (0 << 7) | (scan.value << 6) | (channel << 4) | (mode.value)
 
     def setup_adc(
         self,
-        select: int = 0,
-        clockType: ClockType = ClockType.InternalClock,
+        referenceVoltage: RefrenceVoltage = RefrenceVoltage.InternalReference_AlwaysON_AnalogInput,
+        clock: ClockType = ClockType.Internal,
         polarity: Polarity = Polarity.Unipolar,
-        reset: Reset = Reset.NoAction,
+        reset: ResetMode = ResetMode.NoAction,
+    ) -> None:
+        setup_byte = self._build_setup_byte(referenceVoltage, clock, polarity, reset)
+        self.bus.write_byte(self.address, setup_byte)
+        time.sleep(0.001)
+
+    def read_single(
+        self,
+        channel: int,
+        mode: InputMode = InputMode.SingleEnded,
     ) -> Optional[int]:
         try:
-            setup = self.create_setup_binary(select, clockType, polarity, reset)
-            self.bus.write_byte(self.address, setup)
-        except ValueError as e:
-            print(e)
+            config_byte = self._build_config_byte(ScanMode.NoScan, channel, mode)
+            self.bus.write_byte(self.address, config_byte)
+            time.sleep(0.001)
+            data = self.bus.read_i2c_block_data(self.address, 0x00, 2)
+            return (data[0] << 4) | (data[1] >> 4)
+        except Exception as e:
+            print(f"Read failed: {e}")
             return None
+
+    def read_multiple(
+        self,
+        start_channel: int,
+        count: int,
+        mode: InputMode = InputMode.SingleEnded,
+    ) -> list[int]:
+        if not (0 <= start_channel <= 11) or not (1 <= count <= 12 - start_channel):
+            raise ValueError("Invalid channel range")
+
+        config_byte = self._build_config_byte(
+            ScanMode.ScanUpToChannel, start_channel + count - 1, mode
+        )
+        self.bus.write_byte(self.address, config_byte)
+        time.sleep(0.001)
+
+        result = []
+        for _ in range(count):
+            data = self.bus.read_i2c_block_data(self.address, 0x00, 2)
+            value = (data[0] << 4) | (data[1] >> 4)
+            result.append(value)
+
+        return result
--- whs.py
+++ whs.py
@@ -1,19 +1,81 @@
 import RPi.GPIO as GPIO
 from max1239 import Max1239
+from time import time
+from threading import Thread
+
+# GPIO Pin Mapping
+VALVE_PIN = 17
+
+# ADC Channel Mapping
+CH_HOT = 0
+CH_COLD = 1
+CH_FLOW = 2
+CH_AMBIENT = 4
+
+# ADC stuff
+ADC_VREF = 4.096
+ADC_MAX = 2**12
+
+# Temprature Transmitters Range in Celsius
+T_max = 150
+T_min = -50
+
+# Flowmeter Transmitter Range in g/min
+Q_max = 10
+Q_min = 0.2
 
 # Init Stuff Here
-
 GPIO.setmode(GPIO.BCM)
 
-# Valve Control
-GPIO.setup(17, GPIO.OUT)
+GPIO.setup(VALVE_PIN, GPIO.OUT, initial=GPIO.LOW)
 
-# TODO
-# Display Stuff gotta ask nawaf
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
+# TODO: Display Stuff gotta ask nawaf
+# GPIO.setup(21, GPIO.OUT)
+# GPIO.setup(25, GPIO.OUT)
 
 adc = Max1239()
+adc.setup_adc()
 
-# TODO
-# write the control and logging logic here
+
+def voltage_to_val(V_meas, max, min) -> float:
+    R_shunt = 120
+    I_loop = V_meas / R_shunt
+    return ((I_loop - 4e-3) / (16e-3)) * (max - min) + min
+
+
+def read_voltage(channel: int) -> float:
+    value = adc.read_single(channel)
+    if value is None:
+        return -1
+    return (value / ADC_MAX) * ADC_VREF
+
+
+def draw_water(target_vol: float) -> None:
+    if target_vol <= 0:
+        print("Invalid Volume")
+        return
+
+    print(f"Drawing {target_vol:.2f} gallon(s)")
+
+    volume = 0
+    start_time = time()
+    GPIO.output(VALVE_PIN, GPIO.HIGH)
+
+    while volume < target_vol:
+        elapsed = time() - start_time
+
+        hot = voltage_to_val(read_voltage(CH_HOT), T_max, T_min)
+        cold = voltage_to_val(read_voltage(CH_COLD), T_max, T_min)
+        amb = voltage_to_val(read_voltage(CH_AMBIENT), T_max, T_min)
+        volume = voltage_to_val(read_voltage(CH_FLOW), Q_max, Q_min)
+
+        print(f"{hot:.2f} {cold:.2f} {amb:.2f} {volume:.2f}")
+        if elapsed > 180:
+            print("Timeout Error")
+            break
+
+    GPIO.output(VALVE_PIN, GPIO.LOW)
+    print(f"Volume drawn: {volume:.2f} gallon(s)")
+
+
+thread_draw = Thread(target=draw_water, args=[20])
